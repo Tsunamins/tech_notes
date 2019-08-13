2b5b963635c7b87e6a3497d90350a48435baaf0f
Tsunamins <reillyamr@gmail.com>
2019-08-13 13:25:49 -0400

    add form to signup, show basic info on show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/signup' => 'users#new' #current setup needed slash in front, no quotes around signup_path in home/index - signup link
+  post '/signup' => 'users#create'
   root 'home#index'
   resources :users
 end
