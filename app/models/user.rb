b17c14d5f0ff23abcf6ccc85433e9da5f627e576
Tsunamins <reillyamr@gmail.com>
2019-08-13 14:23:10 -0400

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base 
+    has_secure_password
+
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates :concentration, length: { maximum: 100 }
 end 
