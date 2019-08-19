01de2aae2a166e593c30a69e96c7b12a01288de8
Tsunamins <reillyamr@gmail.com>
2019-08-19 12:55:12 -0400

    add edit user info link, page and route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
         @user = User.find_by(username: params[:user][:username])
             if @user && @user.authenticate(params[:user][:password])
                 session[:user_id] = @user.id
-                redirect_to controller: 'users', action: 'index'
+                redirect_to user_path(@user)
+                #redirect_to controller: 'users', action: 'index'
             else
                 return redirect_to(controller: 'sessions', action: 'new')
             end    
